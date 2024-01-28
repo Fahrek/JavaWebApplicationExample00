21abe3866fe16e8e2b85d954b983d3dc5d666673
Fahrek <andresgqjob@gmail.com>
2024-01-28 10:39:40 +0100

    Recibo de parametros desde MyServlet.java y configuracion de vista en MyServlet2.java.

--- src/main/java/ejemplos/servlet/curso/MyServlet2.java
+++ src/main/java/ejemplos/servlet/curso/MyServlet2.java
@@ -1,6 +1,5 @@
 package ejemplos.servlet.curso;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,17 +10,23 @@ import java.io.PrintWriter;
 @WebServlet(urlPatterns = "/myServlet2")
 public class MyServlet2 extends HttpServlet {
 
+    @Override
     protected void doGet(HttpServletRequest request,
-                         HttpServletResponse response) throws ServletException, IOException {
+                         HttpServletResponse response) throws IOException {
 
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
+
+        String name = request.getParameter("name");
+        String fecha = request.getParameter("date");
 
         // send HTML page to client
         out.println("<html>");
         out.println("<head><title>Ejemplo HTML desde Servlet</title></head>");
         out.println("<body>");
-        out.println("<h1>Bienvenido!!</h1>");
+        out.println("<h3>" + fecha + "</h3>");
+        out.println("<h1>Bienvenida " + name + "!</h1>");
+        out.println("</body></html>");
     }
 
 }
